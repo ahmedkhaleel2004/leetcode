ce65beced578241c4898695708747aa2c3371cc1
Ahmed Khaleel <111161052+ahmedkhaleel2004@users.noreply.github.com>
2024-11-21 07:41:01 -0500

    Update m_71.py

--- python/m_71.py
+++ python/m_71.py
@@ -1,1 +1,14 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
 
+        for directory in path.split("/"):
+            if directory == "..":
+                if stack:
+                    stack.pop()
+            elif directory == "." or not directory:
+                continue
+            else:
+                stack.append(directory)
+        
+        return "/" + "/".join(stack)
